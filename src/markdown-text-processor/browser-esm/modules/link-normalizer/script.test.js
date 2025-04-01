93058ed6dea1d4f9722a793c62d0ca7b300b5794
sleepyshowdy <hojo.miku@gmail.com>
2025-04-01 16:28:03 +0900

    feat: Add DOM integration tests for init() function - Requires testEnvironment: 'jsdom' configuration to enable DOM testing

--- src/markdown-text-processor/browser-esm/modules/link-normalizer/script.test.js
+++ src/markdown-text-processor/browser-esm/modules/link-normalizer/script.test.js
@@ -1,4 +1,4 @@
-import { normalizeLinks } from './script.js'
+import { normalizeLinks, init } from './script.js'
 
 describe('normalizeLinks', () => {
   test('[[A|B]]形式をBに変換', () => {
@@ -17,5 +17,46 @@ describe('normalizeLinks', () => {
 
   test('空文字列をそのまま返す', () => {
     expect(normalizeLinks('')).toBe('')
+  })
+})
+
+/**
+ * @jest-environment jsdom
+ */
+
+describe('init', () => {
+  let container, input, button, output
+
+  beforeEach(() => {
+    // テスト用DOMをセットアップ
+    container = document.createElement('div')
+    container.innerHTML = `
+      <input data-input type="text">
+      <button data-process>Process</button>
+      <div data-output></div>
+    `
+    document.body.appendChild(container)
+
+    input = container.querySelector('[data-input]')
+    button = container.querySelector('[data-process]')
+    output = container.querySelector('[data-output]')
+
+    init(container)
+  })
+
+  afterEach(() => {
+    document.body.removeChild(container)
+  })
+
+  test('ボタンクリックでリンクを正規化', () => {
+    input.value = '[[Hello|World]]'
+    button.click()
+    expect(output.innerHTML).toBe('<pre>Hello</pre>')
+  })
+
+  test('空入力で空の出力', () => {
+    input.value = ''
+    button.click()
+    expect(output.innerHTML).toBe('<pre></pre>')
   })
 })
